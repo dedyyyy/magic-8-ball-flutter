9a116bb30f5d09afabb4402294517bb41da9176e
Dedy <dedyjoomla@gmail.com>
2019-07-01 03:30:45 +0700

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,56 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() => runApp(
-      MaterialApp(
-        home: null,
+  MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.greenAccent,
+      appBar: AppBar(
+        title: Text('Dicee'),
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Ball8(),
+    ),
+  ),
+    );
+
+class Ball8 extends StatefulWidget {
+  @override
+  _Ball8State createState() => _Ball8State();
+}
+
+class _Ball8State extends State<Ball8> {
+
+  int ballNumber = 5;
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                Ubah();
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+
+        ],
       ),
     );
+  }
+
+  void Ubah(){
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+
+
+      print('Button pressed');
+    });
+
+  }
+}
+
